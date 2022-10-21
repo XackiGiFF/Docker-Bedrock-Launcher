8133fe817845a088ccfe51d3b51f754f96fb4b4d
XackiGiFF <m.girsanoff@gmail.com>
2022-10-21 06:42:56 +0300

    first commit

---
+++ start-docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --rm --name mcpe-php --privileged -p 19132:19132/udp -p 19132:19132 -p 19133:19133/udp -p 19133:19133 -v D:\ForWork\DevOps\MPE\Docker\server:/home/mcpe/server xackigiff/mcpe-php:lite ./server/start.sh -l
